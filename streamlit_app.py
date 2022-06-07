8eaaac20097b2f44cb86ff9160d04c742840628f
Krushank Bayyapu <krushankb@gmail.com>
2022-06-07 14:46:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range Eggs')
+
+
+streamlit.header('Build Your Own Fruit Smoothie')
