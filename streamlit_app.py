a81b427d2db4816fcb958d31f8448cc34e83b0f9
Krushank Bayyapu <krushankb@gmail.com>
2022-06-07 14:51:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +10,6 @@ streamlit.text('Hard-boiled free-range Eggs')
 
 
 streamlit.header('Build Your Own Fruit Smoothie')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
