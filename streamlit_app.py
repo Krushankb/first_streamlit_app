64faae4ee1de9ec84846d06db66b3f724cbb0a7c
Krushank Bayyapu <krushankb@gmail.com>
2022-06-08 09:39:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruit_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 fruits_show = my_fruit_list.loc[fruit_selected]
-streamlit.dataframe(fruit_show)
+streamlit.dataframe(fruits_show)
